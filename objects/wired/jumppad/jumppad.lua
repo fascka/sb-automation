5d3dfe03f9866584623694c8f3dc886b4d323644
XspeedPL <xspeedpl@windowslive.com>
2014-01-04 06:37:19 +0100

    Jump Pod finally works

--- objects/wired/jumppad/jumppad.lua
+++ objects/wired/jumppad/jumppad.lua
@@ -1,22 +1,28 @@
 function init(v)
-  if storage.active == nil then storage.active = false end
-  entity.setInteractive(true)
   self.jumpt = 0
 end
 
-function onInteract(args)
-  storage.active = not storage.active
+function firstValidEntity(eids)
+  local valid = { "monster", "npc" }
+  for i, id in ipairs(eids) do
+    local et = world.entityType(eids[i])
+    for j, vt in ipairs(valid) do
+      if et == vt then return id end
+    end
+  end
+  return nil
 end
 
 function main()
   if self.jumpt > 0 then self.jumpt = self.jumpt - 1
-  else--if storage.active then
-    local eids = world.entityQuery(entity.position(), entity.configParameter("objectWidth"), { inSightOf = true, notAnObject = true, order = "nearest" })
-    if #eids > 0 then
-      local e = entityProxy.create(eids[1])
-      --local v = e.velocity()
-      --v[2] = -v[2] * 1.25
-      e.setVelocity({ 0, -20 })
+  else
+    local eids = world.entityQuery(entity.toAbsolutePosition({ 0, 2 }), entity.configParameter("scanRadius"), { notAnObject = true, order = "nearest" })
+    local id = firstValidEntity(eids)
+    if id ~= nil then
+      local e = entityProxy.create(id)
+      local v = e.velocity()
+      v[2] = -v[2] + 50
+      e.setVelocity(v)
       self.jumpt = 10
     end
   end
