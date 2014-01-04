f08f6994c9432823f7f37663b0d566630879c6f7
XspeedPL <xspeedpl@windowslive.com>
2014-01-04 05:15:22 +0100

    Jump Pad done, but Lua fails it all

--- objects/wired/jumppad/jumppad.lua
+++ objects/wired/jumppad/jumppad.lua
@@ -1,6 +1,6 @@
 function init(v)
-  if v then object.setInteractive(true) end
-  if storage.active == nil then storage.active = true end
+  if storage.active == nil then storage.active = false end
+  entity.setInteractive(true)
   self.jumpt = 0
 end
 
@@ -9,19 +9,20 @@ function onInteract(args)
 end
 
 function main()
-  if self.jumpt > 0 then self.jumpt = self.jumpt - 1 end
-  if storage.active then
-    local eids = world.monsterQuery(object.position(), object.configParameter("objectWidth"), { inSightOf = true })
-    for i,id in ipairs(eids) do
-      local v = world.callScriptedEntity(id, "entity.velocity")
-      v[2] = -v[2] * 1.25
-      world.callScriptedEntity(id, "entity.setVelocity", v)
+  if self.jumpt > 0 then self.jumpt = self.jumpt - 1
+  else--if storage.active then
+    local eids = world.entityQuery(entity.position(), entity.configParameter("objectWidth"), { inSightOf = true, notAnObject = true, order = "nearest" })
+    if #eids > 0 then
+      local e = entityProxy.create(eids[1])
+      --local v = e.velocity()
+      --v[2] = -v[2] * 1.25
+      e.setVelocity({ 0, -20 })
       self.jumpt = 10
     end
   end
-  local state = object.animationState("jumpState")
+  local state = entity.animationState("jumpState")
   if state == "idle" and self.jumpt > 0 then
-    object.setAnimationState("jumpState", "jump")
-    object.playImmediateSound(object.configParameter("jumpSound"))
-  elseif state == "jump" and self.jumpt < 1 then object.setAnimationState("jumpState", "idle") end
+    entity.setAnimationState("jumpState", "jump")
+    entity.playImmediateSound(entity.configParameter("jumpSound"))
+  elseif state == "jump" and self.jumpt < 1 then entity.setAnimationState("jumpState", "idle") end
 end
